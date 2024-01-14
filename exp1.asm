36a9bad77240d67b74cd7972e50f8c2cdcfcc5df
BaraAlSedih <98843912+BaraSedih11@users.noreply.github.com>
2024-01-14 23:54:42 +0200

    Create exp1.asm

---
+++ exp1.asm
@@ -0,0 +1,18 @@
+﻿CODE SEGMENT
+ASSUME CS:CODE
+ORG 100H
+
+START:
+
+  mov dx, Off2bh
+  mov al,82h
+  out dx, al
+  
+  read:
+    mov dx,0ff29h
+    in al,dx
+    mov dx, Off28h
+    out dx, al
+    jmp read
+END START
+CODE ENDS
